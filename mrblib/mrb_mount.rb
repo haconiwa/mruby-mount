1deaee74db519f34dd21cc33e663606896c81085
Uchio KONDO <udzura@pepabo.com>
2016-07-12 15:16:08 +0900

    Support some of mount options

--- mrblib/mrb_mount.rb
+++ mrblib/mrb_mount.rb
@@ -4,22 +4,52 @@ class Mount
   end
 
   def mount(source, target, options={})
-    flag = Mount::MS_MGC_VAL
     unless type = options[:type]
       raise ArgumentError, ":type must be specified"
     end
 
+    flag =  MS_MGC_VAL
+    flag |= parse_flags(options)
+
     __mount__(source, target, type, flag, nil)
   end
 
-  def bind_mount(source, target)
-    flag =  Mount::MS_MGC_VAL
-    flag |= Mount::MS_BIND
+  def bind_mount(source, target, options={})
+    flag =  MS_MGC_VAL
+    flag |= MS_BIND
+    flag |= parse_flags(options)
+
     __mount__(source, target, "", flag, nil)
   end
 
   def make_private(target)
-    flag = Mount::MS_PRIVATE
+    flag = MS_PRIVATE
     __mount__("none", target, nil, flag, nil)
+  end
+
+  private
+  def parse_flags(options)
+    if options.is_a? Integer
+      return options
+    end
+
+    flag = 0
+    if options.delete(:readonly)
+      flag |= MS_RDONLY
+    end
+
+    if options.delete(:nosuid)
+      flag |= MS_NOSUID
+    end
+
+    if options.delete(:noexec)
+      flag |= MS_NOEXEC
+    end
+
+    if options.delete(:remount)
+      flag |= MS_REMOUNT
+    end
+
+    return flag
   end
 end
