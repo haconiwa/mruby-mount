8a1e923e1020c7db8a789909f3c9dca46413afa2
Uchio KONDO <udzura@pepabo.com>
2017-01-27 16:48:19 +0900

    Smoke test

--- test/mrb_mount.rb
+++ test/mrb_mount.rb
@@ -3,15 +3,6 @@
 ##
 
 assert("Mount#hello") do
-  t = Mount.new "hello"
-  assert_equal("hello", t.hello)
-end
-
-assert("Mount#bye") do
-  t = Mount.new "hello"
-  assert_equal("hello bye", t.bye)
-end
-
-assert("Mount.hi") do
-  assert_equal("hi!!", Mount.hi)
+  t = Mount.new
+  assert_equal(Mount, t.class)
 end
