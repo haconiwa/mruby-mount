d491d690c9a77567a8d422700a8b7fa72c8d5fe5
matsumotory <matsumotory@pepabo.com>
2017-01-30 15:44:54 +0900

    Add test for bind_mount

--- test/mrb_mount.rb
+++ test/mrb_mount.rb
@@ -6,3 +6,11 @@ assert("Mount class") do
   t = Mount.new
   assert_equal(Mount, t.class)
 end
+
+assert("Mount#bind_mount") do
+  m = Mount.new
+  current_dir = File.expand_path(File.dirname(__FILE__))
+  m.bind_mount("#{current_dir}/test_src_dir", "#{current_dir}/test_dst_dir")
+  assert_true File.exists? "#{current_dir}/test_dst_dir/src"
+  m.umount("#{current_dir}/test_dst_dir")
+end
