bf9ac9890f93e8d6e10cf18cff1ef98f4170f316
matsumotory <matsumotory@pepabo.com>
2017-01-30 15:55:40 +0900

    Add test for Mount#umount

--- test/mrb_mount.rb
+++ test/mrb_mount.rb
@@ -14,3 +14,14 @@ assert("Mount#bind_mount") do
   assert_true File.exists? "#{current_dir}/test_dst_dir/src"
   m.umount("#{current_dir}/test_dst_dir")
 end
+
+assert("Mount#umount") do
+  m = Mount.new
+  current_dir = File.expand_path(File.dirname(__FILE__))
+
+  m.bind_mount("#{current_dir}/test_src_dir", "#{current_dir}/test_dst_dir")
+  assert_true File.exists? "#{current_dir}/test_dst_dir/src"
+
+  m.umount("#{current_dir}/test_dst_dir")
+  assert_true File.exists? "#{current_dir}/test_dst_dir/dst"
+end
